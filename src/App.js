3d89175765c6af8cff8247699b68eea655d39498
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-17 14:37:15 +0530

    added scrolltotop on every page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css';
-import Home from './Components/Home/Home'
-import Navbar from './Components/Navbar/Navbar';
-import Learn from './Components/Learn/Learn'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Learn from "./Components/Learn/Learn";
+import {useState, useEffect} from 'react'
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+
 
 function App() {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+
+  const listenToScroll = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > 40) {
+      !isVisible && setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
   return (
     <Router>
-        <div className="app">
-          <Navbar />
+      <div className="app">
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/learn' element={<Learn/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/learn" element={<Learn />} />
         </Routes>
-    </div>
-      </Router>
+        {isVisible && (
+          <div className="takeBackToTop fixed bottom-16 inline-flex text-[1.5rem] rounded-full z-50 opacity-[.9] right-16 bg-white w-6 h-6 p-8 flex items-center justify-center">
+            <ArrowUpwardIcon
+              onClick={scrollToTop}
+              font-size="small"
+              sx={{ height: "40px", width: "30px" }}
+            />
+          </div>
+        )}
+      </div>
+    </Router>
   );
 }
 
